991b971a40e4aafc84eba94875d892e8705bd047
Valter Costa <costinha_26@hotmail.com>
2022-10-03 23:57:46 +0100

    Work on view model

--- app/src/main/java/com/valterc/ki2/fragments/devices/details/DeviceDetailsViewModel.java
+++ app/src/main/java/com/valterc/ki2/fragments/devices/details/DeviceDetailsViewModel.java
@@ -9,12 +9,20 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.valterc.ki2.data.connection.ConnectionDataInfo;
+import com.valterc.ki2.data.connection.ConnectionStatus;
+import com.valterc.ki2.data.device.BatteryInfo;
 import com.valterc.ki2.data.device.DeviceId;
+import com.valterc.ki2.data.device.SignalInfo;
+import com.valterc.ki2.data.info.DataInfo;
+import com.valterc.ki2.data.info.DataType;
+import com.valterc.ki2.data.info.ManufacturerInfo;
+import com.valterc.ki2.data.shifting.ShiftingInfo;
+import com.valterc.ki2.data.switches.SwitchEvent;
 import com.valterc.ki2.services.IKi2Service;
-import com.valterc.ki2.services.callbacks.IScanCallback;
+import com.valterc.ki2.services.callbacks.IConnectionDataInfoCallback;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Map;
 
 import timber.log.Timber;
 
@@ -24,7 +32,15 @@ public class DeviceDetailsViewModel extends ViewModel {
         @Override
         public void onServiceConnected(ComponentName name, IBinder binder) {
             Timber.d("Service connected");
-            service.postValue(IKi2Service.Stub.asInterface(binder));
+
+            IKi2Service ki2Service = IKi2Service.Stub.asInterface(binder);
+            service.postValue(ki2Service);
+
+            try {
+                ki2Service.registerConnectionDataInfoListener(connectionDataInfoCallback);
+            } catch (RemoteException e) {
+                Timber.e("Unable to register connection data info listener");
+            }
         }
 
         @Override
@@ -34,10 +50,46 @@ public class DeviceDetailsViewModel extends ViewModel {
         }
     };
 
+    private final IConnectionDataInfoCallback connectionDataInfoCallback = new IConnectionDataInfoCallback.Stub() {
+        @Override
+        public void onConnectionDataInfo(DeviceId deviceId, ConnectionDataInfo connectionDataInfo) {
+            connectionStatus.postValue(connectionDataInfo.getConnectionStatus());
+            Map<DataType, DataInfo> dataMap = connectionDataInfo.getDataMap();
+            if (dataMap != null) {
+                postDataIfAvailable(dataMap, DataType.MANUFACTURER_INFO, manufacturerInfo);
+                postDataIfAvailable(dataMap, DataType.SHIFTING, shiftingInfo);
+                postDataIfAvailable(dataMap, DataType.BATTERY, batteryInfo);
+                postDataIfAvailable(dataMap, DataType.SWITCH, switchEvent);
+                postDataIfAvailable(dataMap, DataType.SIGNAL, signalInfo);
+            }
+        }
+    };
+
+    @SuppressWarnings("unchecked")
+    private <TData> void postDataIfAvailable(Map<DataType, DataInfo> dataMap, DataType dataType, MutableLiveData<TData> mutableLiveData) {
+        DataInfo dataInfo;
+        dataInfo = dataMap.get(dataType);
+        if (dataInfo != null && dataInfo.getValue() != null) {
+            mutableLiveData.postValue((TData) dataInfo.getValue());
+        }
+    }
+
     private final MutableLiveData<IKi2Service> service;
+    private final MutableLiveData<ConnectionStatus> connectionStatus;
+    private final MutableLiveData<ManufacturerInfo> manufacturerInfo;
+    private final MutableLiveData<ShiftingInfo> shiftingInfo;
+    private final MutableLiveData<BatteryInfo> batteryInfo;
+    private final MutableLiveData<SwitchEvent> switchEvent;
+    private final MutableLiveData<SignalInfo> signalInfo;
 
     public DeviceDetailsViewModel() {
         this.service = new MutableLiveData<>();
+        connectionStatus = new MutableLiveData<>();
+        manufacturerInfo = new MutableLiveData<>();
+        shiftingInfo = new MutableLiveData<>();
+        batteryInfo = new MutableLiveData<>();
+        switchEvent = new MutableLiveData<>();
+        signalInfo = new MutableLiveData<>();
     }
 
     public ServiceConnection getServiceConnection() {
