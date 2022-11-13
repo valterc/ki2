3cd2e4feb820026fc8b285ab5af6140e81067e67
Valter Costa <costinha_26@hotmail.com>
2022-11-13 21:27:16 +0000

    Perform ride preload

--- app/src/main/java/com/valterc/ki2/karoo/Ki2BroadcastReceiver.java
+++ app/src/main/java/com/valterc/ki2/karoo/Ki2BroadcastReceiver.java
@@ -1,12 +1,18 @@
 package com.valterc.ki2.karoo;
 
 import android.annotation.SuppressLint;
+import android.app.Application;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.util.Log;
+
+import androidx.preference.PreferenceManager;
+
+import com.valterc.ki2.R;
 
 import java.net.URI;
 import java.util.Objects;
@@ -25,26 +31,17 @@ public class Ki2BroadcastReceiver extends BroadcastReceiver {
         switch (intent.getAction()) {
 
             case "android.intent.action.BOOT_COMPLETED":
-                Log.i(TAG, "Boot completed, attempt pre-load");
+                Log.i(TAG, "Boot completed");
 
-                for (int i = 0; i < 2; i++) {
-                    try {
-                        Thread.sleep(2500);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+                boolean preload = preferences.getBoolean(context.getString(R.string.preference_pre_load), false);
+                if (preload) {
+                    Log.i(TAG, "Pre-load enabled, starting ride application");
 
-                    Intent broadcastIntent = new Intent();
-                    broadcastIntent.setAction("io.hammerhead.intent.action.RIDE_APP_PRELOAD");
-                    broadcastIntent.setData(Uri.parse("package:" + context.getPackageName()));
-                    context.sendBroadcast(broadcastIntent);
-
-                    try {
-                        Thread.sleep(15_000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-
+                    Intent intentRideActivity = new Intent(Intent.ACTION_MAIN);
+                    intentRideActivity.setClassName("io.hammerhead.rideapp", "io.hammerhead.rideapp.views.ride.RideActivity");
+                    intentRideActivity.putExtra("ki2.preload", true);
+                    context.startActivity(intentRideActivity);
                 }
 
                 break;
