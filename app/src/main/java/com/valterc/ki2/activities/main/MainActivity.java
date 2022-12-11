8ac2f4fba1e30f2aa7dcd47b2817a2a949e11bcb
Valter Costa <costinha_26@hotmail.com>
2022-12-11 22:40:06 +0000

    Add update overlay after startup

--- app/src/main/java/com/valterc/ki2/activities/main/MainActivity.java
+++ app/src/main/java/com/valterc/ki2/activities/main/MainActivity.java
@@ -1,26 +1,33 @@
 package com.valterc.ki2.activities.main;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView.Adapter;
-import androidx.viewpager2.widget.ViewPager2;
-
-import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView.Adapter;
+import androidx.viewpager2.widget.ViewPager2;
+
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 import com.valterc.ki2.BuildConfig;
 import com.valterc.ki2.R;
-import com.valterc.ki2.fragments.IKarooKeyListener;
 import com.valterc.ki2.data.input.KarooKey;
+import com.valterc.ki2.data.update.UpdateStateStore;
+import com.valterc.ki2.fragments.IKarooKeyListener;
+import com.valterc.ki2.fragments.update.overlay.UpdateOverlayFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int TIME_MS_CHECK_FOR_UPDATES = 3500;
+
+    private final Handler handler = new Handler(Looper.getMainLooper());
     private ViewPager2 viewPager;
 
     @Override
@@ -64,6 +71,21 @@ public class MainActivity extends AppCompatActivity {
 
         ExtendedFloatingActionButton buttonBack = findViewById(R.id.button_back);
         buttonBack.setOnClickListener((view) -> finish());
+
+        if (UpdateStateStore.shouldAutomaticallyCheckForUpdatesInApp(this)) {
+            handler.postDelayed(() -> {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.setCustomAnimations(R.anim.anim_enter_top, R.anim.anim_exit_top, R.anim.anim_enter_top, R.anim.anim_exit_top);
+                transaction.replace(R.id.container_update_overlay, UpdateOverlayFragment.newInstance());
+                transaction.commitNow();
+            }, TIME_MS_CHECK_FOR_UPDATES);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        handler.removeCallbacksAndMessages(null);
     }
 
     @Override
