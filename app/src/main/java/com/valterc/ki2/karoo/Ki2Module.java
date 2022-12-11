388be79b554f6f6673583c9e17be05b331be8a46
Valter Costa <costinha_26@hotmail.com>
2022-12-11 22:14:55 +0000

    Add update notification

--- app/src/main/java/com/valterc/ki2/karoo/Ki2Module.java
+++ app/src/main/java/com/valterc/ki2/karoo/Ki2Module.java
@@ -7,14 +7,17 @@ import androidx.annotation.Nullable;
 
 import com.valterc.ki2.data.message.RideStatusMessage;
 import com.valterc.ki2.data.ride.RideStatus;
+import com.valterc.ki2.karoo.battery.LowBatteryHandler;
 import com.valterc.ki2.karoo.datatypes.BatteryTextDataType;
 import com.valterc.ki2.karoo.datatypes.GearsDrivetrainDataType;
 import com.valterc.ki2.karoo.datatypes.GearsGearsDataType;
 import com.valterc.ki2.karoo.datatypes.GearsTextDataType;
 import com.valterc.ki2.karoo.datatypes.ShiftCountTextDataType;
 import com.valterc.ki2.karoo.datatypes.ShiftModeTextDataType;
+import com.valterc.ki2.karoo.hooks.KarooActivityServiceHook;
 import com.valterc.ki2.karoo.hooks.RideActivityHook;
 import com.valterc.ki2.karoo.service.Ki2ServiceClient;
+import com.valterc.ki2.karoo.update.UpdateAvailableNotification;
 
 import java.util.Arrays;
 import java.util.List;
@@ -42,9 +45,10 @@ public class Ki2Module extends Module {
     public Ki2Module(@NonNull SdkContext context) {
         super(context);
 
-        RideActivityHook.tryHandlePreload(getContext());
+        RideActivityHook.tryHandlePreload(context);
         serviceClient = new Ki2ServiceClient(context);
         ki2Context = new Ki2Context(context, serviceClient);
+        UpdateAvailableNotification.clearUpdateAvailableNotification(context);
     }
 
     @NonNull
