392c6109209d1fc94d5d4d9ff07c4be958e7d874
Valter Costa <costinha_26@hotmail.com>
2022-12-14 18:08:22 +0000

    Removed update

--- app/src/main/java/com/valterc/ki2/update/background/IUpdateCheckerListener.java
+++
@@ -1,9 +0,0 @@
-package com.valterc.ki2.update.background;
-
-import com.valterc.ki2.data.update.ReleaseInfo;
-
-public interface IUpdateCheckerListener {
-
-    void onNewUpdateAvailable(ReleaseInfo releaseInfo);
-
-}
