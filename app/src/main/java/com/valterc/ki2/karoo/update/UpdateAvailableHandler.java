85ae90ade8746a077ae14ec99b54b2292ce7ecea
Valter Costa <costinha_26@hotmail.com>
2023-01-01 11:46:34 +0000

    Fix handlers (#80)
    * Fix weak references being GC'ed
    
    * Renamed method

--- app/src/main/java/com/valterc/ki2/karoo/update/UpdateAvailableHandler.java
+++ app/src/main/java/com/valterc/ki2/karoo/update/UpdateAvailableHandler.java
@@ -7,14 +7,19 @@ import com.valterc.ki2.data.message.UpdateAvailableMessage;
 import com.valterc.ki2.karoo.Ki2Context;
 import com.valterc.ki2.karoo.handlers.IRideHandler;
 
+import java.util.function.Consumer;
+
+@SuppressWarnings("FieldCanBeLocal")
 @SuppressLint("LogNotTimber")
 public class UpdateAvailableHandler implements IRideHandler {
 
     private final Ki2Context context;
 
+    private final Consumer<UpdateAvailableMessage> onUpdateAvailableMessage = this::onUpdateAvailableMessage;
+
     public UpdateAvailableHandler(Ki2Context context) {
         this.context = context;
-        context.getServiceClient().getCustomMessageClient().registerUpdateAvailableWeakListener(this::onUpdateAvailableMessage);
+        context.getServiceClient().getCustomMessageClient().registerUpdateAvailableWeakListener(onUpdateAvailableMessage);
     }
 
     private void onUpdateAvailableMessage(UpdateAvailableMessage updateAvailableMessage) {
