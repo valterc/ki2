ff9cfe4066d601ed809991224669ec40e336a4d5
Valter Costa <costinha_26@hotmail.com>
2023-01-03 17:14:41 +0000

    Add Front and Rear gear data elements (#82)

---
+++ app/src/main/java/com/valterc/ki2/karoo/datatypes/FrontGearDataType.java
@@ -0,0 +1,88 @@
+package com.valterc.ki2.karoo.datatypes;
+
+import android.graphics.drawable.Drawable;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.content.res.AppCompatResources;
+
+import com.valterc.ki2.R;
+import com.valterc.ki2.karoo.Ki2Context;
+import com.valterc.ki2.karoo.formatters.FrontGearFormatter;
+import com.valterc.ki2.utils.CallerUtils;
+
+import java.util.Collections;
+import java.util.List;
+
+import io.hammerhead.sdk.v0.datatype.formatter.SdkFormatter;
+import io.hammerhead.sdk.v0.datatype.transformer.BuiltInTransformer;
+import io.hammerhead.sdk.v0.datatype.transformer.SdkTransformer;
+import io.hammerhead.sdk.v0.datatype.view.BuiltInView;
+import io.hammerhead.sdk.v0.datatype.view.SdkView;
+
+public class FrontGearDataType extends Ki2DataType {
+
+    private static final String TYPE_ID = "ki2::front-gear-text";
+
+    private final List<Drawable> drawables;
+
+    public FrontGearDataType(@NonNull Ki2Context context) {
+        super(context);
+
+        this.drawables = CallerUtils.safeWrap(() -> Collections.singletonList(
+                AppCompatResources.getDrawable(getContext(), R.drawable.ic_hh_gear)));
+    }
+
+    @NonNull
+    @Override
+    public String getDescription() {
+        return "Front gear index.";
+    }
+
+    @NonNull
+    @Override
+    public String getDisplayName() {
+        return "Front Gear";
+    }
+
+    @NonNull
+    @Override
+    public String getTypeId() {
+        return TYPE_ID;
+    }
+
+    @Nullable
+    @Override
+    public List<Drawable> displayIcons() {
+        return drawables;
+    }
+
+    @Override
+    public double getSampleValue() {
+        return 1;
+    }
+
+    @NonNull
+    @Override
+    public SdkTransformer newTransformer() {
+        return getKi2Context()
+                .getInstanceManager()
+                .getOrComputeInstance(BuiltInTransformer.Identity.class.getSimpleName(), SdkTransformer.class, () -> new BuiltInTransformer.Identity(getContext()));
+    }
+
+    @NonNull
+    @Override
+    public SdkFormatter newFormatter() {
+        return getKi2Context()
+                .getInstanceManager()
+                .getOrComputeInstance(FrontGearFormatter.class.getSimpleName(), SdkFormatter.class, () -> new FrontGearFormatter(getKi2Context()));
+    }
+
+    @NonNull
+    @Override
+    public SdkView newView() {
+        return getKi2Context()
+                .getInstanceManager()
+                .getOrComputeInstance(BuiltInView.Numeric.class.getSimpleName(), SdkView.class, () -> new BuiltInView.Numeric(getContext()));
+    }
+}
