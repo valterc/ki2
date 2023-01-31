fcff46762977ac792823746f796d8d2e7382ebc2
Valter Costa <costinha_26@hotmail.com>
2023-01-31 19:24:23 +0000

    Format

--- app/src/main/java/com/valterc/ki2/ant/connection/handler/profile/ShimanoEBikeProfileHandler.java
+++ app/src/main/java/com/valterc/ki2/ant/connection/handler/profile/ShimanoEBikeProfileHandler.java
@@ -4,7 +4,7 @@ import com.valterc.ki2.ant.connection.IDeviceConnectionListener;
 import com.valterc.ki2.ant.connection.handler.transport.ITransportHandler;
 import com.valterc.ki2.data.device.DeviceId;
 
-public class ShimanoEBikeProfileHandler extends ShimanoShiftingProfileHandler{
+public class ShimanoEBikeProfileHandler extends ShimanoShiftingProfileHandler {
 
     public ShimanoEBikeProfileHandler(DeviceId deviceId, ITransportHandler transportHandler, IDeviceConnectionListener deviceConnectionListener) {
         super(deviceId, transportHandler, deviceConnectionListener);
