e3d651cc83682094e026c35c338adfd30c4d011b
Valter Costa <costinha_26@hotmail.com>
2023-03-21 09:53:36 +0000

    Add docs

--- app/src/main/java/com/valterc/ki2/data/shifting/ShiftingLimitType.java
+++ app/src/main/java/com/valterc/ki2/data/shifting/ShiftingLimitType.java
@@ -1,11 +1,23 @@
 package com.valterc.ki2.data.shifting;
 
+/**
+ * Shifting limit type.
+ */
 public enum ShiftingLimitType {
 
+    /**
+     * Not on shifting limit.
+     */
     NONE,
 
+    /**
+     * Lower shifting limit, both front and rear gears at min.
+     */
     LOWER_LIMIT,
 
+    /**
+     * Higher shifting limit, both front and rear gears at max.
+     */
     HIGHER_LIMIT
 
 }
