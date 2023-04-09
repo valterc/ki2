becf80a7a0b00626e462756f39586efb771ae9d4
Valter Costa <costinha_26@hotmail.com>
2023-04-09 10:12:42 +0100

    Add overlay simple theme (#151)

--- app/src/main/java/com/valterc/ki2/karoo/overlay/view/builder/OverlayViewBuilderRegistry.java
+++ app/src/main/java/com/valterc/ki2/karoo/overlay/view/builder/OverlayViewBuilderRegistry.java
@@ -5,6 +5,8 @@ import com.valterc.ki2.karoo.overlay.view.plain.DefaultDarkGearIndexOverlayView;
 import com.valterc.ki2.karoo.overlay.view.plain.DefaultDarkGearSizeRatioOverlayView;
 import com.valterc.ki2.karoo.overlay.view.plain.DefaultLightGearIndexOverlayView;
 import com.valterc.ki2.karoo.overlay.view.plain.DefaultLightGearSizeRatioOverlayView;
+import com.valterc.ki2.karoo.overlay.view.simple.SimpleDarkOverlayView;
+import com.valterc.ki2.karoo.overlay.view.simple.SimpleLightOverlayView;
 
 import java.util.HashMap;
 
@@ -16,10 +18,12 @@ public final class OverlayViewBuilderRegistry {
     private static final HashMap<String, OverlayViewBuilderEntry> builderMap = new HashMap<>();
 
     static {
-        builderMap.put("default_light_gear_size_ratio", new OverlayViewBuilderEntry(R.layout.view_karoo_overlay, DefaultLightGearSizeRatioOverlayView::new));
-        builderMap.put("default_dark_gear_size_ratio", new OverlayViewBuilderEntry(R.layout.view_karoo_overlay, DefaultDarkGearSizeRatioOverlayView::new));
-        builderMap.put("default_light_gear_index", new OverlayViewBuilderEntry(R.layout.view_karoo_overlay, DefaultLightGearIndexOverlayView::new));
         builderMap.put("default_dark_gear_index", new OverlayViewBuilderEntry(R.layout.view_karoo_overlay, DefaultDarkGearIndexOverlayView::new));
+        builderMap.put("default_light_gear_index", new OverlayViewBuilderEntry(R.layout.view_karoo_overlay, DefaultLightGearIndexOverlayView::new));
+        builderMap.put("default_dark_gear_size_ratio", new OverlayViewBuilderEntry(R.layout.view_karoo_overlay, DefaultDarkGearSizeRatioOverlayView::new));
+        builderMap.put("default_light_gear_size_ratio", new OverlayViewBuilderEntry(R.layout.view_karoo_overlay, DefaultLightGearSizeRatioOverlayView::new));
+        builderMap.put("simple_dark", new OverlayViewBuilderEntry(R.layout.view_karoo_overlay, SimpleDarkOverlayView::new));
+        builderMap.put("simple_light", new OverlayViewBuilderEntry(R.layout.view_karoo_overlay, SimpleLightOverlayView::new));
     }
 
     public static OverlayViewBuilderEntry getBuilder(String key) {
