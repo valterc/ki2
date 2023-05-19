ace74781ac96155224c40dd34bb47e03ba356427
Valter Costa <costinha_26@hotmail.com>
2023-05-19 11:14:31 +0100

    Add more battery fields

---
+++ app/src/main/java/com/valterc/ki2/karoo/datatypes/BatteryStatusTextDataType.java
@@ -0,0 +1,89 @@
+package com.valterc.ki2.karoo.datatypes;
+
+import android.graphics.drawable.Drawable;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.content.res.AppCompatResources;
+
+import com.valterc.ki2.R;
+import com.valterc.ki2.karoo.Ki2Context;
+import com.valterc.ki2.karoo.formatters.BatteryStatusTextFormatter;
+import com.valterc.ki2.utils.function.FunctionUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
+import io.hammerhead.sdk.v0.datatype.formatter.SdkFormatter;
+import io.hammerhead.sdk.v0.datatype.transformer.BuiltInTransformer;
+import io.hammerhead.sdk.v0.datatype.transformer.SdkTransformer;
+import io.hammerhead.sdk.v0.datatype.view.BuiltInView;
+import io.hammerhead.sdk.v0.datatype.view.SdkView;
+
+public class BatteryStatusTextDataType extends Ki2DataType {
+
+    private static final String TYPE_ID = "ki2::battery-status-text";
+
+    private final List<Drawable> drawables;
+
+    public BatteryStatusTextDataType(@NonNull Ki2Context context) {
+        super(context);
+
+        this.drawables = FunctionUtils.safeInvoke(() -> Arrays.asList(
+                AppCompatResources.getDrawable(getContext(), R.drawable.ic_hh_battery), AppCompatResources.getDrawable(getContext(), R.drawable.ic_hh_gear)));
+    }
+
+    @NonNull
+    @Override
+    public String getDescription() {
+        return "Shifting battery status.";
+    }
+
+    @NonNull
+    @Override
+    public String getDisplayName() {
+        return "Battery";
+    }
+
+    @NonNull
+    @Override
+    public String getTypeId() {
+        return TYPE_ID;
+    }
+
+    @Nullable
+    @Override
+    public List<Drawable> displayIcons() {
+        return drawables;
+    }
+
+    @Override
+    public double getSampleValue() {
+        return 80;
+    }
+
+    @NonNull
+    @Override
+    public SdkTransformer newTransformer() {
+        return getKi2Context()
+                .getInstanceManager()
+                .getOrComputeInstance(BuiltInTransformer.Identity.class.getSimpleName(), SdkTransformer.class, () -> new BuiltInTransformer.Identity(getContext()));
+    }
+
+    @NonNull
+    @Override
+    public SdkFormatter newFormatter() {
+        return getKi2Context()
+                .getInstanceManager()
+                .getOrComputeInstance(BatteryStatusTextFormatter.class.getSimpleName(), SdkFormatter.class, () -> new BatteryStatusTextFormatter(getKi2Context()));
+    }
+
+    @NonNull
+    @Override
+    public SdkView newView() {
+        return getKi2Context()
+                .getInstanceManager()
+                .getOrComputeInstance(BuiltInView.Numeric.class.getSimpleName(), SdkView.class, () -> new BuiltInView.Numeric(getContext()));
+    }
+
+}
