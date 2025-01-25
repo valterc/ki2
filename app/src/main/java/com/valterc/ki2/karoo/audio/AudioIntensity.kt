4bf41ad842de4d10c02c757471c250c68374b2c3
Valter Costa <costinha_26@hotmail.com>
2025-01-25 12:04:02 +0000

    Adjust audio intensity for Karoo 3

--- app/src/main/java/com/valterc/ki2/karoo/audio/AudioIntensity.kt
+++ app/src/main/java/com/valterc/ki2/karoo/audio/AudioIntensity.kt
@@ -1,11 +1,26 @@
 package com.valterc.ki2.karoo.audio
 
-enum class AudioIntensity(val frequencyMultiplier: Double, val durationMultiplier: Double) {
+enum class AudioIntensity(
+    val frequencyMultiplierK2: Double, val durationMultiplierK2: Double,
+    val frequencyMultiplierK24: Double, val durationMultiplierK24: Double,
+    ) {
 
-    Quiet(0.01, 0.7),
-    Low(0.1, 0.9),
-    Reduced(0.4, 0.9),
-    Normal(1.0, 1.0);
+    Quiet(
+        0.01, 0.7,
+        0.05, 0.3
+    ),
+    Low(
+        0.1, 0.9,
+        0.1, 0.4
+    ),
+    Reduced(
+        0.4, 0.9,
+        0.4, 0.4
+    ),
+    Normal(
+        1.0, 1.0,
+        1.0, 0.5
+    );
 
     companion object {
         fun fromName(name: String): AudioIntensity {
